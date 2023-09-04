de50eb4764eb94bf9af960f64c5863403c01bfbd
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-09-04 21:48:42 +0500

    .

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ import {
 import { checkAuth, handleValidationErrors } from "./utils/index.js";
 import {UserController, PostController} from "./controllers/index.js";
 
-
-
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('DB ok'))
 .catch((err) => console.log('DB error', err));
